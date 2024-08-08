21d0ef522892552f03549ccd72a3afd28c755695
Mwan3kii <agathawanja303@gmail.com>
2024-08-08 21:13:42 +0300

    Need meeting

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -7,8 +7,6 @@ BEGIN
     SELECT AVG(score) INTO avg_score
     FROM corrections
     WHERE user_id = user_id;
-
-    -- Update user's average_score
     UPDATE users
     SET average_score = avg_score
     WHERE id = user_id;
