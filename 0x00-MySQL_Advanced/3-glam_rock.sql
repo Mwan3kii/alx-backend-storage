bbc1afd03202e0bb28b22c0e7c295d3878ac9ba5
Mwan3kii <agathawanja303@gmail.com>
2024-08-08 22:01:15 +0300

    Correct ouput

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,5 +1,5 @@
 -- Create table if not exists
 SELECT band_name, (IFNULL(split, '2020') - formed) AS lifespan
     FROM metal_bands
-    WHERE FIND_IN_SET('Glam rock', IFNULL(style, "")) > 0
+    WHERE FIND_IN_SET('Glam rock', IFNULL(main_style, "")) > 0
     ORDER BY lifespan DESC;
