eb51074aa2c9118a501a96f3a70b4110fd64efcb
Mwan3kii <agathawanja303@gmail.com>
2024-08-08 19:22:10 +0300

    add commits

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,7 +1,4 @@
-CREATE DATABASE IF NOT EXISTS holberton;
-
-USE holberton;
-
+--creates a table users
 CREATE TABLE IF NOT EXISTS users (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
