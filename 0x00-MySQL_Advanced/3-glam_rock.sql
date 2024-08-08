33d778d088603cf0cf710f37f1571c79d536b746
Mwan3kii <agathawanja303@gmail.com>
2024-08-08 21:35:56 +0300

    Fix ouput

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,13 +1,12 @@
--- List all band with glam rock
-SELECT 
-    name AS band_name,
-    CASE 
-        WHEN split IS NULL THEN 2022 - formed
-        ELSE split - formed
-    END AS lifespan
-FROM 
-    metal_bands
-WHERE 
-    style = 'Glam rock'
-ORDER BY 
-    lifespan DESC;
+-- Create table if not exists
+
+CREATE TABLE IF NOT EXISTS metal_bands (
+    band_name VARCHAR(255),
+    main_style VARCHAR(255),
+    formed YEAR,
+    split YEAR
+)
+SELECT band_name, (IFNULL(split, '2020') - formed) AS lifespan
+    FROM metal_bands
+    WHERE FIND_IN_SET('Glam rock', IFNULL(style, "")) > 0
+    ORDER BY lifespan DESC;
